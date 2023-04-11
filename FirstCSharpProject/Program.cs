46394daf3f70297d87205a8812883d95fad068f8
Ennosukeblade <mouadhjenouiz@gmail.com>
2023-04-11 18:01:02 +0100

    Debugging practice

--- FirstCSharpProject/Program.cs
+++ FirstCSharpProject/Program.cs
@@ -1,29 +1,38 @@
-﻿// Data types
-// String
-string UserName = "alex";
-Console.WriteLine(UserName);
-// Integer
-int AlexAge = 25;
-Console.WriteLine(value: AlexAge);
-// Float = 32bits
-float Rate = 1.20f;
-// Decimal = 64bits
-decimal tax = 1.5015M;
+﻿// // Data types
+// // String
+// string UserName = "alex";
+// Console.WriteLine(UserName);
+// // Integer
+// int AlexAge = 25;
+// Console.WriteLine(value: AlexAge);
+// // Float = 32bits
+// float Rate = 1.20f;
+// // Decimal = 64bits
+// decimal tax = 1.5015M;
 
-// * Data Structure
-// Array
-int[] FirstArray = new int[5] { 1, 2, 3, 4, 5 };
-Console.WriteLine(value: FirstArray);
-float[] SecondArray = { 1.25f, 0.89f, 5.75f };
-// List
-// New object or new instance of the class List
-List<string> names = new List<string>();
-names.Add(item: UserName);
-names.Add("15");
-names.Add("Sarah");
-names.Remove("15");
-// you can put the index: or not
-names.RemoveAt(index: 1);
+// // * Data Structure
+// // Array
+// int[] FirstArray = new int[5] { 1, 2, 3, 4, 5 };
+// Console.WriteLine(value: FirstArray);
+// float[] SecondArray = { 1.25f, 0.89f, 5.75f };
+// // List
+// // New object or new instance of the class List
+// List<string> names = new List<string>();
+// names.Add(item: UserName);
+// names.Add("15");
+// names.Add("Sarah");
+// names.Remove("15");
+// // you can put the index: or not
+// names.RemoveAt(index: 1);
 
-Console.WriteLine(value: names[0]);
+// Console.WriteLine(value: names[0]);
 
+Console.WriteLine("My name is {0}, I am " + 28 + " years old", "Tim");
+Console.WriteLine($"My name is {0}, I am " + 28 + " years old", "Tim");
+
+Random rand = new Random();
+for (int val = 0; val < 10; val++)
+{
+    //Prints the next random value between 2 and 8
+    Console.WriteLine(rand.Next(2, 8));
+}
---
+++ Language Fundamentals/Practice/debugging-practice/DebuggingPractice/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Challenge 1
+bool amProgrammer = true; // "true" is a string and the variable must be boolean
+int Age = 27;
+List<string> Names = new List<string>();
+//Names = "Monica"; // we can't assign a string type to a list type
+Names.Add("Monica");
+//Console.WriteLine(Names[0]);
+Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
+MyDictionary.Add("Hello", "0");
+//MyDictionary.Add("Hi there", 0); // key value in the MyDictionary are both strings we cannot add an integer value
+// This is a tricky one! Hint: look up what a char is in C#
+string MyName = 'MyName'; // single quote is for a single character...
+// Challenge 2
+List<int> Numbers = new List<int>() { 2, 3, 6, 7, 1, 5 };
+for (int i = Numbers.Count - 1; i >= 0; i--)
+{
+    Console.WriteLine(Numbers[i]); // i must less than List length
+}
+// Challenge 3
+List<int> MoreNumbers = new List<int>() { 12, 7, 10, -3, 9 };
+foreach (int i in MoreNumbers)
+{
+    //Console.WriteLine(MoreNumbers[i]);
+    // "i" is the value not the index, here we tried to print MoreNumbers[12] 12 is out of range
+    Console.WriteLine(i);
+}
+// Challenge 4
+List<int> EvenMoreNumbers = new List<int> { 3, 6, 9, 12, 14 };
+/* foreach (int num in EvenMoreNumbers)
+{
+    if (num % 3 == 0)
+    {
+        num = 0; // num is an iteration variable, we cannot assign a variable to it
+        
+    }
+} */
+for (int i = 0; i < EvenMoreNumbers.Count; i++)
+{
+    if (EvenMoreNumbers[i] % 3 == 0)
+    {
+        EvenMoreNumbers[i] = 0;
+    }
+}
+Console.WriteLine(EvenMoreNumbers[2]);
+// Challenge 5
+// What can we learn from this error message?
+string MyString = "superduberawesome";
+MyString[7] = "p"; // String objects are immutable 
+// Challenge 6
+// Hint: some bugs don't come with error messages
+Random rand = new Random();
+int randomNum = rand.Next(12); // 12 is not included in the range of randomNum
+if (randomNum == 12) // This condition will never happen
+{
+    Console.WriteLine("Hello");
+}
+
