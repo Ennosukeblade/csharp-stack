15ccdb35e4d846da6505873e6ad4bc619b896d4c
Ennosukeblade <mouadhjenouiz@gmail.com>
2023-04-15 15:40:37 +0100

    Boxing / Unboxing

---
+++ Language Fundamentals/Practice/boxing-unboxing/Program.cs
@@ -0,0 +1,17 @@
+﻿List<object> MyObjects = new List<object>();
+MyObjects.Add(7);
+MyObjects.Add(28);
+MyObjects.Add(-1);
+MyObjects.Add(true);
+MyObjects.Add("chair");
+int sum = 0;
+foreach (var item in MyObjects)
+{
+    System.Console.WriteLine(item);
+    if (item is int)
+    {
+        sum += (int)item;
+    }
+}
+System.Console.WriteLine(sum);
+
